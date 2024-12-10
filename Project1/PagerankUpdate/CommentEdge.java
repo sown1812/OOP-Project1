0d505f48f30e8f9ab82bda47b23b3ec6823243af
KeyVN <72498419+KeyVN@users.noreply.github.com>
2024-12-10 16:49:39 +0700

    Delete Project1/PagerankUpdate directory

--- Project1/PagerankUpdate/CommentEdge.java
+++
@@ -1,7 +0,0 @@
-package pagerankup;
-
-public class CommentEdge extends Edge {
-    public CommentEdge(String from, String to) {
-        super(from, to, 0.7, "comment");
-    }
-}
--- Project1/PagerankUpdate/RetweetEdge.java
+++
@@ -1,7 +0,0 @@
-package pagerankup;
-
-public class RetweetEdge extends Edge {
-    public RetweetEdge(String from, String to) {
-        super(from, to, 0.9, "retweet");
-    }
-}
